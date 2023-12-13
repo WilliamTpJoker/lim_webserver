6231299a7e8387db96da327fb247e7ab0c96c874
lim <lzj985391695@163.com>
2023-12-13 12:56:55 +0800

    支持序列化

---
+++ src/bytearray.cpp
@@ -0,0 +1,26 @@
+#include "bytearray.h"
+#include "log.h"
+
+
+namespace lim_webserver
+{
+    static Logger::ptr g_logger = LIM_LOG_NAME("system");
+
+ByteArray::Node::Node(size_t s)
+    :ptr(new char[s])
+    ,next(nullptr)
+    ,size(s) {
+}
+
+ByteArray::Node::Node()
+    :ptr(nullptr)
+    ,next(nullptr)
+    ,size(0) {
+}
+
+ByteArray::Node::~Node() {
+    if(ptr) {
+        delete[] ptr;
+    }
+}
+} // namespace lim_webserver
